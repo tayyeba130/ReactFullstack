208b8797033baadbe133bd8a7ae073701bd12a9e
tayyeba130 <tayyebashoaib08@gmail.com>
2022-01-11 22:40:50 +0000

    7 - Comment Section

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,14 +5,42 @@ import { useParams } from 'react-router-dom';
 function Post() {
 	const { id } = useParams();
 	const [post, setPost] = useState(null);
+	const [comments, setComments] = useState([]);
+	const [newComment, setNewComment] = useState('');
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
-			console.log(response.data);
 			setPost(response.data);
+			setLoading(false);
 		});
 	}, [id]);
 
+	useEffect(() => {
+		axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+			setComments(response.data);
+		});
+	}, [id]);
+
+	const addComment = async () => {
+		axios
+			.post('http://localhost:3001/comments', {
+				commentBody: newComment,
+				PostId: id
+			})
+			.then(() => {
+				setNewComment("");
+				axios
+					.get(`http://localhost:3001/comments/${id}`)
+					.then((response) => {
+						setComments(response.data);
+					});
+			});
+	};
+
+	if (loading) {
+		return 'Loading...';
+	}
 	return (
 		<div className="postPage">
 			<div className="leftSide">
@@ -22,7 +50,26 @@ function Post() {
 					<div className="footer">{post.userName}</div>
 				</div>
 			</div>
-			<div className="rightSide">Comment section</div>
+			<div className="rightSide">
+				<div className="addCommentContainer">
+					<input
+						type="text"
+						placeholder="Comment"
+						onChange={(e) => setNewComment(e.target.value)}
+						value={newComment}
+					/>
+					<button onClick={addComment}>Add comment</button>
+				</div>
+				<div className="listOfComments">
+					{comments.map((comment) => {
+						return (
+							<div className="comment" key={comment.id}>
+								{comment.commentBody}
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 }
